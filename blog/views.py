3e5b429274cd566f70f6236054bfa1ac933371f2
Aman Chaudhary <aman8601964624@gmail.com>
2021-04-14 17:43:28 +0530

    First

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import BProject
+
+
+def all_blogs(request):
+    projects = BProject.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'projects': projects})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(BProject, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
